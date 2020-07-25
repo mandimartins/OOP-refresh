0866e8361fca5d8a515cf2326b805ce35ffe8b2f
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-07-25 18:29:55 -0300

    Do basic operations and show on the screen

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -42,23 +42,54 @@ class CalcController {
     return ['+', '-', '*', '%', '/'].indexOf(value) > -1;
   }
 
+  pushOperation(value) {
+    this._operation.push(value);
+
+    if (this._operation.length > 3) {
+      this.calc();
+    }
+  }
+
+  calc() {
+    let last = this._operation.pop();
+    let result = eval(this._operation.join(''));
+    this._operation = [result, last];
+    this.setLastNumberToDisplay();
+  }
+
+  setLastNumberToDisplay() {
+    let lastNumber;
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+      if (!this.isOperator(this._operation[i])) {
+        lastNumber = this._operation[i];
+        break;
+      }
+    }
+
+    this.displayCalc = lastNumber;
+  }
+
   addOperation(value) {
     if (isNaN(this.getLastOperation())) {
       if (this.isOperator(value)) {
         this.setLastOperation(value);
       } else if (isNaN(value)) {
         //outra coisa
+        console.log('OUTRA COISA', value);
       } else {
-        this._operation.push(value);
+        this.pushOperation(value);
+        this.setLastNumberToDisplay();
       }
     } else {
-      let newValue = this.getLastOperation().toString() + value.toString();
-      this.setLastOperation(parseInt(newValue));
+      if (this.isOperator(value)) {
+        this.pushOperation(value);
+      } else {
+        let newValue = this.getLastOperation().toString() + value.toString();
+        this.setLastOperation(parseInt(newValue));
+
+        this.setLastNumberToDisplay();
+      }
     }
-
-    this._operation.push(value);
-
-    console.log(this._operation);
   }
 
   setError() {
